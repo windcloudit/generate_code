13942f925148cf995ec6554994a165a3f803264f
tat.pham <tat.pham@persol.co.jp>
2020-06-16 11:26:08 +0700

    #Finish change commposer json

--- src/Commands/GenerateRepository/RepositoryGenerator.php
+++ src/Commands/GenerateRepository/RepositoryGenerator.php
@@ -8,7 +8,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace WindCloud\GenerateCode;
+namespace WindCloud\GenerateCode\GenerateRepository;
+
+use WindCloud\GenerateCode\Builder;
 
 class RepositoryGenerator
 {
