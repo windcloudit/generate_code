9c204009f92b1ee6952cba391cdfde5c7a93e0ca
tat.pham <tat.pham@persol.co.jp>
2020-06-18 11:18:28 +0700

    #Update composer

--- src/Commands/GenerateService.php
+++ src/Commands/GenerateService.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace WindCloud\GenerateCode;
+namespace WindCloud\GenerateCode\Commands;
 
 use WindCloud\GenerateCode\GenerateController\ControllerGenerator;
 use WindCloud\GenerateCode\GenerateRouter\RouterGenerator;
